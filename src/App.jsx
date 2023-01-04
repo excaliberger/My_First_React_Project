834be95e600b97106a8c53e773859f237f99a62d
Emily Berger <excaliberger@gmail.com>
2023-01-03 19:04:29 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello World!</h1>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             name="text"
